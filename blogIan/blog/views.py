c39901f827788325c97f1e69b7e36cdf71066180
Ian Cossa <ianrafaelelias@gmail.com>
2026-01-10 00:36:10 +0530

    Creating functions

--- blogIan/blog/views.py
+++ blogIan/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404  
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Category, Post
+
 
 # Create your views here.
+from .models import Post
+from .form import PostForm
+
+#Home
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+#Post Detail
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+
